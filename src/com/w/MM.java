489d53590d6ff1774261f85078c21aa8fbcd3752
蔡雨鑫 <1261148380@qq.com>
2020-11-24 16:03:05 +0800

    第一次..

---
+++ src/com/w/MM.java
@@ -0,0 +1,12 @@
+package com.w;
+
+/**
+ * 作者：cyx
+ * 日期: 2020/11/24 16:00
+ * 描述:
+ */
+public class MM {
+    public static void main(String[] args) {
+        System.out.println("vvvvvvvv");
+    }
+}
